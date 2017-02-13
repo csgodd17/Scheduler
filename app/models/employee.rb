4b102c6a02cf42f972242baf1a97f414f47a6529
csgodd17 <csgod17@vt.edu>
2017-02-13 11:23:41 -0500

    Add controllers for employees and projects

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,9 @@
 class Employee < ApplicationRecord
   has_many :teams
   has_many :projects, through: :teams
+
+  def team(project_id)
+    team_object = Team.where(project_id: project_id).where(employee_id: self.id)
+    return team_object
+  end
 end
